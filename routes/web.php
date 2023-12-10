faaf452b25756ed80aca8a81c5fe772611682f99
Tesfamariam Teshome <tesfumerry16@gmail.com>
2023-12-10 20:14:56 +0300

    signup route added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users', function () {
-    return view('user.index');
-});
+Route::get('/register/seeker', [UserController::class, 'createSeeker'])->name('create.seeker');
+Route::post('/register/seeker', [UserController::class, 'storeSeeker'])->name('store.seeker');
